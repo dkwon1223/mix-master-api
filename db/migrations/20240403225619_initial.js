585f12facbc558a7baaa310ee874b451c62d413c
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-04-03 17:23:40 -0600

    Add initial migration

---
+++ db/migrations/20240403225619_initial.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema 
+    .createTable('beverages', function (table) {
+        table.increments('id').primary();
+        table.string('name').notNullable;
+        table.specificType('ingredients', 'text ARRAY');
+        table.boolean('alcoholic');
+        table.timestamps(true, true);
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema
+    .dropTable('beverages');
+};
