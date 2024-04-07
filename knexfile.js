d628521368aae76df58ccf3dfa91380e47c03dab
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-04-07 01:46:56 -0600

    Add environment variables with dotenv and implement AWS RDS PostgreSQL database

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,6 @@
 // Update with your config settings.
 
+require('dotenv').config();
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
@@ -7,7 +8,16 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/drinks',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    },
     migrations: {
       directory: './db/migrations'
     },
