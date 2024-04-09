1bbaf5e02daf5ae712f96acb74179fb40119471f
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-04-09 16:57:11 -0600

    Change port

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const environment = process.env.NODE_ENV || 'production';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+
+app.set('port', process.env.PORT || 10000);
+app.locals.title = "Mix Master API";
+
+app.get('/', (request, response) => {
+    response.send("Thirsty? Let's find a drink");
+});
+
+app.get('/api/v1/drinks', async (request, response) => {
+    try {
+        const drinks = await database('beverages').select();
+        response.status(200).json(drinks);
+    } catch(error) {
+        response.status(500).json({error});
+    }
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
