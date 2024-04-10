a244f91f21cc3fed0b70316ddc066725d7b817a1
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-04-09 18:39:17 -0600

    Add build command configuration

--- knexfile.js
+++ knexfile.js
@@ -6,17 +6,30 @@ require('dotenv').config();
  */
 module.exports = {
 
-  // development: {
-  //   client: 'pg',
-  //   connection: 'postgres://localhost/drinks',
-  //   migrations: {
-  //     directory: './db/migrations'
-  //   },
-  //   seeds: {
-  //     directory: './db/seeds'
-  //   },
-  //   useNullAsDefault: true
-  // },
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
+    },
+    useNullAsDefault: true
+  },
 
   // staging: {
   //   client: 'postgresql',
