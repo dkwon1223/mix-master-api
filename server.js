9c9afd38ae7711109dae60061f17d09458fafdf0
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-04-09 20:10:24 -0600

    Add dynamic endpoint for fetching drink by id

--- server.js
+++ server.js
@@ -24,6 +24,19 @@ app.get('/api/v1/drinks', async (request, response) => {
     }
 });
 
+app.get('/api/v1/drinks/:id', async (request, response) => {
+    try {
+        const id = parseInt(request.params.id);
+        const drinks = await database('beverages').select();
+        const targetDrink = drinks.filter((drink) => {
+            return drink.id === id
+        })
+        response.status(200).json(targetDrink);
+    } catch(error) {
+        response.status(500).json({error});
+    }
+});
+
 app.listen(app.get('port'), () => {
     console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 });
