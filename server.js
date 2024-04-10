1d60b1d776ea8738d466d9d3ec55906f781da80f
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-04-09 18:44:07 -0600

    Add env requirement in server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 const environment = process.env.NODE_ENV || 'production';
 const configuration = require('./knexfile')[environment];
