220ca864bdf022382aa9f04d3691fdcd4f065205
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-04-09 18:10:48 -0600

    Add pool to knexfile

--- knexfile.js
+++ knexfile.js
@@ -46,6 +46,10 @@ module.exports = {
         rejectUnauthorized: false
       }
     },
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
       directory: './db/migrations'
     },
