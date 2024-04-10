ebe823a9d6918032c1c474aa3f52c7d9ac8c15cb
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-04-09 19:56:19 -0600

    Add cors to express dependencies

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
+app.use(cors());
 
 const environment = process.env.NODE_ENV || 'production';
 const configuration = require('./knexfile')[environment];
